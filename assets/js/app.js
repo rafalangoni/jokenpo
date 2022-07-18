756fb3455c7879d5f9244338248bb2ceff51b4da
Rafael Langoni <langoninaves@gmail.com>
2022-07-18 12:09:45 -0300

    projeto fnalizado com tema escuro e claro

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ const pedra = document.querySelector("[data-tipo=pedra]");
 const papel = document.querySelector("[data-tipo=papel]");
 const tesoura = document.querySelector("[data-tipo=tesoura]");
 const computador = ['Papel', 'Pedra', 'Tesoura'];
+const temaClaro = document.querySelector(".tema-claro");
+const temaEscuro = document.querySelector(".tema-escuro");
 let vencedor = document.querySelector('.principal__vencedor')
 let escolhaComputador = document.querySelector('.principal__computador-escolha');
 
@@ -45,10 +47,17 @@ tesoura.addEventListener("click", () => {
         vencedor.textContent = ' Computador venceu!';
         escolhaComputador.textContent = ' Pedra';
     } else if (valorDoComputador == 0) {
-        vencedor.textContent = ' Você enceu!';
+        vencedor.textContent = ' Você venceu!';
         escolhaComputador.textContent = ' Papel';
     }else{
         vencedor.textContent = ' Empate!';
         escolhaComputador.textContent = ' Tesoura';
     }
+})
+
+temaEscuro.addEventListener("click", () => {
+    document.querySelector('.principal').classList.add('principal--dark');
+})
+temaClaro.addEventListener("click", () => {
+    document.querySelector('.principal').classList.remove('principal--dark');
 })
